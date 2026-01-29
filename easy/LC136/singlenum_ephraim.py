17868ea48ba331491159e429b01177d002926a24
ephraaimwong <73452838+ephraaimwong@users.noreply.github.com>
2026-01-29 15:16:52 -0600

    LC136

--- easy/LC136/singlenum_ephraim.py
+++ easy/LC136/singlenum_ephraim.py
@@ -6,7 +6,7 @@ class Solution(object):
         """
         """
         ---- Solution Intent: XOR ----
-        Since constraits are O(n) run and O(1) space, XOR bit manipulation is best.
+        Since constraints are O(n) run and O(1) space, XOR bit manipulation is best.
         
         elem XOR elem = 0
         if there is only 1 non-repeated elem with all else repeated twice, XOR all elem will cancel out all double repeated.
